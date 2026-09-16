b124132d22230a7b52a8f7f7a469ccc18eb096c7
CarlosMG0715 <carlosmanrique1234v@gmail.com>
2026-09-15 21:40:33 -0500

    Agregar modelo Producto con atributos básicos para la gestión de inventario

--- app/models.py
+++ app/models.py
@@ -11,4 +11,14 @@ class Cliente(db.Model):
 
     ventas = db.relationship('Venta', backref='cliente', lazy=True)
 
-
+class Producto(db.Model):
+    __tablename__='producto'
+    
+    id_producto = db.Column(db.Integer, primary_key=True)
+    descripcion = db.Column(db.String(150), nullable=False)
+    categoria = db.Column(db.String(100))
+    precio = db.Column(db.Numeric(12, 2), nullable=False)
+    stock = db.Column(db.Integer, default=0)
+    stock_minimo = db.Column(db.Integer, default=0)
+    
+    
