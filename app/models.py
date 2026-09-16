e0b3f73226ddac79d043c18cc8e80a7a840dc0ea
CarlosMG0715 <carlosmanrique1234v@gmail.com>
2026-09-15 21:34:19 -0500

    Creacion de tabla clientes

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from app.database import db
+
+class Cliente(db.Model):
+    __tablename__ = 'cliente'
+
+    id_cliente = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    direccion = db.Column(db.String(150))
+    telefono = db.Column(db.String(20))
+    email = db.Column(db.String(100))
+
+    ventas = db.relationship('Venta', backref='cliente', lazy=True)
+
+
